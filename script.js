adecaf4b300be738a45da06a0e9308f38235179a
Mohannad-133 <mohannad.web133@gmail.com>
2026-09-06 18:46:27 +0200

    Erste Demo-Website

---
+++ script.js
@@ -0,0 +1,10 @@
+const contactButton =
+    document.getElementById("contactButton");
+
+contactButton.addEventListener("click", function () {
+
+    alert(
+        "Dies ist eine Demo-Website. Hier könnte später ein echtes Buchungssystem geöffnet werden."
+    );
+
+});
